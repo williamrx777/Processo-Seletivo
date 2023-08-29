7b5a384f6f2ecacd2549ddc47812e02a8e681a90
william rodrigues <williamr723@gmail.com>
2023-08-29 16:08:42 -0300

    integrando os candidatos selecionados

--- candidatura/src/main/java/org/candidatura/ProcessoSeletivo.java
+++ candidatura/src/main/java/org/candidatura/ProcessoSeletivo.java
@@ -7,7 +7,21 @@ public class ProcessoSeletivo {
         //analisarCandidato(1900.0);
         //analisarCandidato(2200.0);
         //analisarCandidato(2000.0);
-        selecaoDeCandidatos();
+        //selecaoDeCandidatos();
+        imprimirSelecionados();
+    }
+
+    static void imprimirSelecionados() {
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO","AUGUSTO"};
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+
+        for(int indice=0; indice < candidatos.length; indice++) {
+           System.out.println("O candidato de nº " + (indice+1) + " é " + candidatos[indice]);
+        }
+        System.out.println("Forma abreviada de interação for each");
+        for(String candidato : candidatos) {
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
     }
 
     static void selecaoDeCandidatos(){
